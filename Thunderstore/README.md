633617b9b8dfde72c76e231553e89ab2cdc2cadd
Onyx <50289874+Tizitendo@users.noreply.github.com>
2026-09-16 09:38:13 +0200

    evis ignore allies

--- Thunderstore/README.md
+++ Thunderstore/README.md
@@ -1,6 +1,7 @@
 # NoDronePush
 
-Drones now phase through you
+* Drones now phase through you
+* Merc eviscerate now passes through allies
 
 ## Contact
 For questions or bug reports, you can find me in the [RoR2 Modding Server](https://discord.gg/YxEdVK7xWM) @Onyx
--- src/NoDronePush.cs
+++ src/NoDronePush.cs
@@ -1,6 +1,11 @@
+using System;
 using BepInEx;
+using EntityStates.Merc;
 using Logger;
+using Mono.Cecil.Cil;
+using MonoMod.Cil;
 using RoR2;
+using UnityEngine;
 
 [assembly: HG.Reflection.SearchableAttribute.OptIn]
 
@@ -32,5 +37,49 @@ public sealed class NoDronePush : BaseUnityPlugin
 				drone.bodyPrefab.layer = LayerIndex.playerFakeActor.intVal;
 			}
 		}
+
+		IL.EntityStates.Merc.EvisDash.FixedUpdate += EvisDash_FixedUpdate;
 	}
+
+    private static void EvisDash_FixedUpdate(ILContext il)
+    {
+        ILCursor c = new(il);
+		ILLabel label = null;
+		int hurtboxLoc = 0;
+
+		if (!c.TryGotoNext(
+			x => x.MatchCallOrCallvirt(typeof(Component), nameof(Component.GetComponent)),
+			x => x.MatchStloc(out hurtboxLoc)
+		))
+		{
+			Log.Error(il.Method.Name + " IL Hook failed!");
+		}
+
+		c.Index = 0;
+
+		if (c.TryGotoNext(
+			x => x.MatchNewobj(typeof(EntityStates.Merc.Evis))
+		) &&
+		c.TryGotoPrev(MoveType.After,
+			x => x.MatchBrfalse(out label)
+		))
+		{
+			c.Emit(OpCodes.Ldarg_0);
+			c.Emit(OpCodes.Ldloc, hurtboxLoc);
+			c.EmitDelegate<Func<EvisDash, HurtBox, bool>>(ignoreTeamMates);
+			c.Emit(OpCodes.Brfalse, label);
+		} else
+		{
+			Log.Error(il.Method.Name + " IL Hook failed!");
+		}
+
+		static bool ignoreTeamMates(EvisDash self, HurtBox hurtBox)
+		{
+			if (TeamMask.GetUnprotectedTeams(self.GetTeam()).HasTeam(hurtBox.teamIndex))
+			{
+				return true;
+			}
+			return false;
+		}
+    }
 }
